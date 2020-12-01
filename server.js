117ba181c587b59af54f1c27fa524889692f06f2
edalvb <edalvb@gmail.com>
2020-11-30 19:49:49 -0500

    cambios en el server

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
-
-app.listen(process.env.PORT || 8080);
+app.use(express.static(__dirname + '/dist/client'));
 
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname + 'dist/client/index.html'));
 });
+
+app.listen(process.env.PORT || 8080);
 
 console.log('Console of Client "EBE TEAM" Listening');
