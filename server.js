1dcf21ee4d4856b5f634dcb31038e35580ab6557
edalvb <edalvb@gmail.com>
2020-11-30 20:05:54 -0500

    cambios en el package.js y server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 app.use(express.static(__dirname + '/dist/client'));
 
-app.get('/*', function(req, res) {
+app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname + 'dist/client/index.html'));
 });
 
