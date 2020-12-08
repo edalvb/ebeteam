13fa17529007a7af65a09391854556fb5dfa7ea8
Blanca Estaurofila <blank.esj@gmail.com>
2020-12-08 00:09:23 -0500

    aliniacion de cards del home

--- client/src/app/components/canal-osiptel/canal-atencion-osiptel/canal-atencion-osiptel.component.ts
+++ client/src/app/components/canal-osiptel/canal-atencion-osiptel/canal-atencion-osiptel.component.ts
@@ -2,6 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { MatIconRegistry } from '@angular/material/icon';
 import { CanalAtencionOsiptel } from "../../../modelo/osiptel";
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DialogCanalDesOsiptelComponent } from "../../dialog/dialog-canal-des-osiptel/dialog-canal-des-osiptel.component";
+
+export interface DialogData {
+  animal: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-canal-atencion-osiptel',
@@ -9,6 +16,11 @@ import { CanalAtencionOsiptel } from "../../../modelo/osiptel";
   styleUrls: ['./canal-atencion-osiptel.component.css']
 })
 export class CanalAtencionOsiptelComponent implements OnInit {
+
+  // dialog: any;
+
+  animal: string = '';
+  name: string = '';
 
   canales: CanalAtencionOsiptel[] = [
     {
@@ -37,13 +49,15 @@ export class CanalAtencionOsiptelComponent implements OnInit {
     },
   ]
 
-  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer, public dialog: MatDialog) {
     this.agregarIcono('presencial', iconRegistry, sanitizer);
     this.agregarIcono('redes_sociales', iconRegistry, sanitizer);
     this.agregarIcono('web', iconRegistry, sanitizer);
     this.agregarIcono('telefonico', iconRegistry, sanitizer);
     this.agregarIcono('electronico', iconRegistry, sanitizer);
     this.agregarIcono('radial', iconRegistry, sanitizer);
+
+    // this.dialog = new DialogCanalDesOsiptelComponent();
   }
 
   ngOnInit(): void { }
@@ -52,6 +66,19 @@ export class CanalAtencionOsiptelComponent implements OnInit {
     iconRegistry.addSvgIcon(
       nombre,
       sanitizer.bypassSecurityTrustResourceUrl(`assets/img/${nombre}.svg`));
+  }
+
+  openDialog(): void {
+
+    const dialogRef = this.dialog.open(DialogCanalDesOsiptelComponent, {
+      width: '250px',
+      data: { name: this.name, animal: this.animal }
+    });
+
+    dialogRef.afterClosed().subscribe((result: any) => {
+      console.log('The dialog was closed');
+      this.animal = result;
+    });
   }
 
 }
--- client/src/app/components/dialog/dialog-canal-des-osiptel/dialog-canal-des-osiptel.component.ts
+++ client/src/app/components/dialog/dialog-canal-des-osiptel/dialog-canal-des-osiptel.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+export interface DialogData {
+  animal: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-dialog-canal-des-osiptel',
@@ -7,9 +13,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogCanalDesOsiptelComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public dialogRef: MatDialogRef<any>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) { }
 
   ngOnInit(): void {
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 
 }
