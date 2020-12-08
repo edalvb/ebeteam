a1e0d8034ccd77e6c0c9026227bc0fb0da4f778c
Blanca Estaurofila <blank.esj@gmail.com>
2020-12-07 19:00:31 -0500

    prototipo de home

--- client/src/app/components/denuncias/denuncias.component.ts
+++ client/src/app/components/denuncias/denuncias.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Denuncia {
+  atributo: string;
+  descripcion: string;
+}
+
 @Component({
   selector: 'app-denuncias',
   templateUrl: './denuncias.component.html',
@@ -7,6 +12,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DenunciasComponent implements OnInit {
 
+  denuncias: Denuncia[] = [
+    {
+      atributo: 'Sede',
+      descripcion: 'Los Olivos ',
+    },
+    {
+      atributo: 'N° Denuncia',
+      descripcion: '001-2019-LN/DEN',
+    },
+    {
+      atributo: 'Fecha Presentación',
+      descripcion: '8/01/2019',
+    },
+    {
+      atributo: 'Empresa Operadora',
+      descripcion: 'TELEFÓNICA DEL PERÚ S.A.A.',
+    },
+    {
+      atributo: 'Recurso',
+      descripcion: 'Incumplimiento de resolución de primera instancia',
+    },
+    {
+      atributo: 'Servicio',
+      descripcion: 'Servicios empaquetados',
+    },
+    {
+      atributo: 'Estado',
+      descripcion: 'CONCLUIDO',
+    },
+    {
+      atributo: 'Materia',
+      descripcion: 'Derechos reconocidos',
+    },
+  ];
   constructor() { }
 
   ngOnInit(): void {
