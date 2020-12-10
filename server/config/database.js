c89cda137d3be1db147c3c9c0e9ebdb0e6f36476
edalvb <edalvb@gmail.com>
2020-12-09 19:27:55 -0500

    compatibilidad con MySQL

--- server/config/database.js
+++ server/config/database.js
@@ -1,39 +1,39 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'mongoose',
-      settings: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        srv: env.bool('DATABASE_SRV', false),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'ebeteam'),
-        username: env('DATABASE_USERNAME', null),
-        password: env('DATABASE_PASSWORD', null),
-      },
-      options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', false),
-      },
-    },
-  },
-});
-
 // module.exports = ({ env }) => ({
 //   defaultConnection: 'default',
 //   connections: {
 //     default: {
-//       connector: 'bookshelf',
+//       connector: 'mongoose',
 //       settings: {
-//         client: 'mysql',
 //         host: env('DATABASE_HOST', '127.0.0.1'),
-//         port: env.int('DATABASE_PORT', 3306),
+//         srv: env.bool('DATABASE_SRV', false),
+//         port: env.int('DATABASE_PORT', 27017),
 //         database: env('DATABASE_NAME', 'ebeteam'),
-//         username: env('DATABASE_USERNAME', 'ebeteam'),
-//         password: env('DATABASE_PASSWORD', 'ebe-team1'),
-//         ssl: env.bool('DATABASE_SSL', true),
+//         username: env('DATABASE_USERNAME', null),
+//         password: env('DATABASE_PASSWORD', null),
 //       },
-//       options: {}
+//       options: {
+//         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+//         ssl: env.bool('DATABASE_SSL', false),
+//       },
 //     },
 //   },
 // });
+
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'mysql',
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'ebeteam'),
+        username: env('DATABASE_USERNAME', 'ebeteam'),
+        password: env('DATABASE_PASSWORD', 'ebe-team1'),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
+      options: {}
+    },
+  },
+});
