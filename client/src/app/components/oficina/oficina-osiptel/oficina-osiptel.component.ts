395454ce2362b72d34408874523e9e34957e1dc1
Edward Alexis <edalvb@gmail.com>
2020-12-11 22:50:00 +0000

    Traida de oficinas en canales de atención

--- client/src/app/components/oficina/oficina-osiptel/oficina-osiptel.component.ts
+++ client/src/app/components/oficina/oficina-osiptel/oficina-osiptel.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy } from '@angular/core';
-import { OficinaOsiptel } from "../../../modelo/osiptel";
 import * as ubicacionQL from "../../../graphql/ubicacion";
+import * as sedeQL from "../../../graphql/sede";
 import { Apollo } from 'apollo-angular';
 import { Subscription } from 'rxjs';
 
@@ -11,13 +11,16 @@ import { Subscription } from 'rxjs';
 })
 export class OficinaOsiptelComponent implements OnDestroy {
 
+  idDepa: any = 0;
+
   panelOpenState = false;
 
   departamentos: any;
 
-  oficinas: OficinaOsiptel[] = [];
+  oficinas: any;
 
   private qSubDepartamento: Subscription;
+  private qSubSede: any;
 
   constructor(private apollo: Apollo) {
     this.qSubDepartamento = this.apollo
@@ -30,8 +33,22 @@ export class OficinaOsiptelComponent implements OnDestroy {
       });
   }
 
+  getSedes() {
+    console.log(this.idDepa)
+    this.qSubSede = this.apollo.watchQuery({
+      query: sedeQL.GET_SEDES_X_DEPARTAMENTO,
+      variables: {
+        ide: this.idDepa
+      }
+    }).valueChanges.subscribe(({ data }) => {
+      let dato: any = data;
+      this.oficinas = dato.departamento.sedes;
+    })
+  }
+
   ngOnDestroy() {
     this.qSubDepartamento.unsubscribe();
+    this.qSubSede ? this.qSubSede.unsubscribe() : "";
   }
 
 }
