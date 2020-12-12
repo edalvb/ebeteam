08bf0f1079ca9bd9468f7fa4aa21743b01ef23b6
Blanca Estaurofila <blank.esj@gmail.com>
2020-12-11 23:56:47 -0500

    sol error en app.component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material/icon';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+// import { AuthService } from "./services/auth.service";
 
 @Component({
   selector: 'app-root',
@@ -8,7 +14,54 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public child = "1";
 
-  funChild(e: any){
-    this.child=e;
+  cambiaTema: boolean = false;
+  url = 'https://img.ecartelera.com/noticias/57100/57138-m.jpg';
+
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    iconRegistry: MatIconRegistry, sanitizer: DomSanitizer
+    /*private authService: AuthService*/) {
+    this.agregarIcono('osiptel', iconRegistry, sanitizer),
+      this.agregarIcono('logo', iconRegistry, sanitizer)
   }
+
+  funChild(e: any) {
+    this.child = e;
+  }
+
+  agregarIcono(nombre: string, iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon(
+      nombre,
+      sanitizer.bypassSecurityTrustResourceUrl(`/assets/img/${nombre}.svg`));
+  }
+
+  setStyles() {
+    return {
+      'background-image': `url(${this.url})`
+    };
+  }
+
+  cambia(e: any) {
+    this.cambiaTema = e;
+  }
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  accede() {
+    // return this.authService.acceder()
+  }
+
+  cerrarSesion() {
+    // this.authService.cerrarSesion();
+  }
+
+  cambiarTema() {
+    this.cambiaTema = !this.cambiaTema;
+  }
+
+
 }
